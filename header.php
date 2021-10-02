3d14054f3b6789242c3217cd20b9662bbb6c9caf
tobimelvin <djololotoi@gmail.com>
2021-10-02 18:13:33 +0200

    mise en ligne de l'index html

--- header.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="shortcut icon" href="files/img/favicon.ico" type="image/x-ico">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="files/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Groupe-4</title>
-</head>
